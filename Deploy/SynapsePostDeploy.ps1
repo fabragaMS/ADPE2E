f7ce9a8e4c9d0818dfc8e85e3079436addd8c7af
fabragaMS <50192911+fabragaMS@users.noreply.github.com>
2022-03-25 16:49:01 +1000

    Add files via upload

--- Deploy/SynapsePostDeploy.ps1
+++ Deploy/SynapsePostDeploy.ps1
@@ -153,139 +153,132 @@ function Save-SynapseSampleArtifacts{
 
   foreach($sampleArtifactCollection in $sampleCodeIndex)
   {
-      Write-Host "Loop Collection: $($sampleArtifactCollection.template)"
-      if ($sampleArtifactCollection.template -eq $SampleArtifactCollectionName) {
-          Write-Host "Deploying Sample Artifact Collection: $($sampleArtifactCollection.template)"
-          Write-Host "-----------------------------------------------------------------------"
+    Write-Host "Loop Collection: $($sampleArtifactCollection.template)"
+    if ($sampleArtifactCollection.template -eq $SampleArtifactCollectionName) {
+      Write-Host "Deploying Sample Artifact Collection: $($sampleArtifactCollection.template)"
+      Write-Host "-----------------------------------------------------------------------"
 
-          #Create SQL Script artifacts.
-          Write-Host "Deploying SQL Scripts:"
-          Write-Host "-----------------------------------------------------------------------"
-          foreach($sqlScript in $sampleArtifactCollection.artifacts.sqlScripts)
-          {
-              $definitionFilePath = $sqlScript.definitionFilePath
-              $fileContent = Invoke-WebRequest $definitionFilePath
+      #Create SQL Script artifacts.
+      Write-Host "Deploying SQL Scripts:"
+      Write-Host "-----------------------------------------------------------------------"
+      foreach($sqlScript in $sampleArtifactCollection.artifacts.sqlScripts)
+      {
+        $fileContent = Invoke-WebRequest $sqlScript.definitionFilePath
 
-              if ($sqlScript.tokens.length -gt 0) {
-                  foreach($token in $sqlScript.tokens)
-                  {
-                      $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
-                  }
-              }
+        if ($sqlScript.tokens.length -gt 0) {
+            foreach($token in $sqlScript.tokens)
+            {
+                $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
+            }
+        }
 
-              $definitionFilePath = [guid]::NewGuid()
-              Set-Content -Path $definitionFilePath $fileContent
-              Set-AzSynapseSqlScript -WorkspaceName $SynapseWorkspaceName -Name $sqlScript.name -DefinitionFile $definitionFilePath -FolderPath $sqlScript.workspaceFolderPath
-              Remove-Item -Path $definitionFilePath
-          }
-
-          #Create Linked Service artifacts.
-          Write-Host "Deploying Linked Service:"
-          Write-Host "-----------------------------------------------------------------------"
-          foreach($linkedService in $sampleArtifactCollection.artifacts.linkedServices)
-          {
-              $definitionFilePath = $linkedService.definitionFilePath
-              $fileContent = Invoke-WebRequest $definitionFilePath
-
-              if ($linkedService.tokens.length -gt 0) {
-                  foreach($token in $linkedService.tokens)
-                  {
-                      $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
-                  }
-              }
-
-              $definitionFilePath = [guid]::NewGuid()
-              Set-Content -Path $definitionFilePath $fileContent 
-              Set-AzSynapseLinkedService -WorkspaceName $SynapseWorkspaceName -Name $linkedService.name -DefinitionFile $definitionFilePath 
-              Remove-Item -Path $definitionFilePath
-          }
-
-          #Create Dataset artifacts.
-          Write-Host "Deploying Datasets:"
-          Write-Host "-----------------------------------------------------------------------"
-          foreach($dataset in $sampleArtifactCollection.artifacts.datasets)
-          {
-              $definitionFilePath = $dataset.definitionFilePath
-              $fileContent = Invoke-WebRequest $definitionFilePath
-
-              if ($dataset.tokens.length -gt 0) {
-                  foreach($token in $dataset.tokens)
-                  {
-                      $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
-                  }
-              }
-
-              $definitionFilePath = [guid]::NewGuid()
-              Set-Content -Path $definitionFilePath $fileContent
-              Set-AzSynapseDataset -WorkspaceName $SynapseWorkspaceName -Name $dataset.name -DefinitionFile $definitionFilePath
-              Remove-Item -Path $definitionFilePath
-          }
-
-          #Create Dataflows artifacts.
-          Write-Host "Deploying Dataflows:"
-          Write-Host "-----------------------------------------------------------------------"
-          foreach($dataflow in $sampleArtifactCollection.artifacts.dataflows)
-          {
-              $definitionFilePath = $dataflow.definitionFilePath
-              $fileContent = Invoke-WebRequest $definitionFilePath
-
-              if ($dataflow.tokens.length -gt 0) {
-                  foreach($token in $dataflow.tokens)
-                  {
-                      $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
-                  }
-              }
-
-              $definitionFilePath = [guid]::NewGuid()
-              Set-Content -Path $definitionFilePath $fileContent
-              Set-AzSynapseDataFlow -WorkspaceName $SynapseWorkspaceName -Name $dataflow.name -DefinitionFile $definitionFilePath
-              Remove-Item -Path $definitionFilePath
-          }
-
-          #Create Pipeline artifacts.
-          Write-Host "Deploying Pipelines:"
-          Write-Host "-----------------------------------------------------------------------"
-          foreach($pipeline in $sampleArtifactCollection.artifacts.pipelines)
-          {
-              $definitionFilePath = $pipeline.definitionFilePath
-              $fileContent = Invoke-WebRequest $definitionFilePath
-
-              if ($pipeline.tokens.length -gt 0) {
-                  
-                  foreach($token in $pipeline.tokens)
-                  {
-                      $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
-                  }
-              }
-
-              $definitionFilePath = [guid]::NewGuid()
-              Set-Content -Path $definitionFilePath $fileContent
-              Set-AzSynapsePipeline -WorkspaceName $SynapseWorkspaceName -Name $pipeline.name -DefinitionFile $definitionFilePath
-              Remove-Item -Path $definitionFilePath
-          }
-
-          #Create Notebook artifacts.
-          Write-Host "Deploying Notebooks:"
-          Write-Host "-----------------------------------------------------------------------"
-          foreach($notebook in $sampleArtifactCollection.artifacts.notebooks)
-          {
-              $definitionFilePath = $notebook.definitionFilePath
-              $fileContent = Invoke-WebRequest $definitionFilePath
-
-              if ($notebook.tokens.length -gt 0) {
-                  
-                  foreach($token in $notebook.tokens)
-                  {
-                      $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
-                  }
-              }
-
-              $definitionFilePath = [guid]::NewGuid()
-              Set-Content -Path $definitionFilePath $fileContent
-              Set-AzSynapseNotebook -WorkspaceName $SynapseWorkspaceName -Name $notebook.name -DefinitionFile $definitionFilePath -FolderPath $notebook.workspaceFolderPath
-              Remove-Item -Path $definitionFilePath
-          }
+        $definitionFilePath = [guid]::NewGuid()
+        Set-Content -Path $definitionFilePath $fileContent
+        Set-AzSynapseSqlScript -WorkspaceName $SynapseWorkspaceName -Name $sqlScript.name -DefinitionFile $definitionFilePath -FolderPath $sqlScript.workspaceFolderPath
+        Remove-Item -Path $definitionFilePath
       }
+
+      #Create Linked Service artifacts.
+      Write-Host "Deploying Linked Service:"
+      Write-Host "-----------------------------------------------------------------------"
+      foreach($linkedService in $sampleArtifactCollection.artifacts.linkedServices)
+      {
+        $fileContent = Invoke-WebRequest $linkedService.definitionFilePath
+
+        if ($linkedService.tokens.length -gt 0) {
+            foreach($token in $linkedService.tokens)
+            {
+                $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
+            }
+        }
+
+        $definitionFilePath = [guid]::NewGuid()
+        Set-Content -Path $definitionFilePath $fileContent 
+        Set-AzSynapseLinkedService -WorkspaceName $SynapseWorkspaceName -Name $linkedService.name -DefinitionFile $definitionFilePath 
+        Remove-Item -Path $definitionFilePath
+      }
+
+      #Create Dataset artifacts.
+      Write-Host "Deploying Datasets:"
+      Write-Host "-----------------------------------------------------------------------"
+      foreach($dataset in $sampleArtifactCollection.artifacts.datasets)
+      {
+        $fileContent = Invoke-WebRequest $dataset.definitionFilePath
+
+        if ($dataset.tokens.length -gt 0) {
+            foreach($token in $dataset.tokens)
+            {
+                $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
+            }
+        }
+
+        $definitionFilePath = [guid]::NewGuid()
+        Set-Content -Path $definitionFilePath $fileContent
+        Set-AzSynapseDataset -WorkspaceName $SynapseWorkspaceName -Name $dataset.name -DefinitionFile $definitionFilePath
+        Remove-Item -Path $definitionFilePath
+      }
+
+      #Create Dataflows artifacts.
+      Write-Host "Deploying Dataflows:"
+      Write-Host "-----------------------------------------------------------------------"
+      foreach($dataflow in $sampleArtifactCollection.artifacts.dataflows)
+      {
+        $fileContent = Invoke-WebRequest $dataflow.definitionFilePath
+
+        if ($dataflow.tokens.length -gt 0) {
+            foreach($token in $dataflow.tokens)
+            {
+                $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
+            }
+        }
+
+        $definitionFilePath = [guid]::NewGuid()
+        Set-Content -Path $definitionFilePath $fileContent
+        Set-AzSynapseDataFlow -WorkspaceName $SynapseWorkspaceName -Name $dataflow.name -DefinitionFile $definitionFilePath
+        Remove-Item -Path $definitionFilePath
+      }
+
+      #Create Pipeline artifacts.
+      Write-Host "Deploying Pipelines:"
+      Write-Host "-----------------------------------------------------------------------"
+      foreach($pipeline in $sampleArtifactCollection.artifacts.pipelines)
+      {
+        $fileContent = Invoke-WebRequest $pipeline.definitionFilePath
+
+        if ($pipeline.tokens.length -gt 0) {
+            
+            foreach($token in $pipeline.tokens)
+            {
+                $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
+            }
+        }
+
+        $definitionFilePath = [guid]::NewGuid()
+        Set-Content -Path $definitionFilePath $fileContent
+        Set-AzSynapsePipeline -WorkspaceName $SynapseWorkspaceName -Name $pipeline.name -DefinitionFile $definitionFilePath
+        Remove-Item -Path $definitionFilePath
+      }
+
+      #Create Notebook artifacts.
+      Write-Host "Deploying Notebooks:"
+      Write-Host "-----------------------------------------------------------------------"
+      foreach($notebook in $sampleArtifactCollection.artifacts.notebooks)
+      {
+        $fileContent = Invoke-WebRequest $notebook.definitionFilePath
+
+        if ($notebook.tokens.length -gt 0) {
+          foreach($token in $notebook.tokens)
+          {
+              $fileContent = $fileContent -replace $token, $synapseTokens.Get_Item($token)
+          }
+        }
+
+        $definitionFilePath = [guid]::NewGuid()
+        Set-Content -Path $definitionFilePath $fileContent
+        Set-AzSynapseNotebook -WorkspaceName $SynapseWorkspaceName -Name $notebook.name -DefinitionFile $definitionFilePath -FolderPath $notebook.workspaceFolderPath
+        Remove-Item -Path $definitionFilePath
+      }
+    }
   }
 }
 
